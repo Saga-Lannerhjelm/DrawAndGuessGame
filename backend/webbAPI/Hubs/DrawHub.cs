2d422164129b692e764eeda831a62a95a6421d5a
Saga-Lannerhjelm <saga.lannerhjelm@icloud.com>
2024-12-16 15:18:30 +0100

    Draw on canvas with SignalR

--- backend/webbAPI/Hubs/DrawHub.cs
+++ backend/webbAPI/Hubs/DrawHub.cs
@@ -9,8 +9,16 @@ namespace webbAPI.Hubs
 {
     public class DrawHub : Hub
     {
-        public async Task Drawing(Point start, Point end) {
-            await Clients.Others.SendAsync("Drawing", start, end);
+
+        // public override Task OnConnectedAsync()
+        // {
+        //     Groups.AddToGroupAsync(Context.ConnectionId, "Test");
+        //     Clients.Group("Test").SendAsync("Message", $"{Context.ConnectionId} har anslutit till grupp Test");
+        //     return base.OnConnectedAsync();
+        // }
+        
+        public async Task Drawing(Point start, Point end, string color) {
+            await Clients.Others.SendAsync("Drawing", start, end, color);
         }
     }
 }
--- frontend/react-app/src/components/Game/Game.jsx
+++ frontend/react-app/src/components/Game/Game.jsx
@@ -1,76 +1,101 @@
 import React, { useEffect, useRef, useState } from "react";
+import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
 
 const Game = () => {
   const [color, setColor] = useState("#ffffff");
-  const [loading, setLoading] = useState(true);
+  const [connection, setConnection] = useState(undefined);
   const canvasRef = useRef(null);
 
-  const getColor = async () => {
+  async function startConnection() {
+    if (!connection) {
+      const connection = new HubConnectionBuilder()
+        .withUrl("http://localhost:5034/draw")
+        .configureLogging(LogLevel.Information)
+        .withAutomaticReconnect()
+        .build();
+
+      connection.on("Drawing", (start, end, color) => {
+        drawStroke(start, end, color);
+      });
+
+      try {
+        await connection.start();
+        setConnection(connection);
+      } catch (error) {
+        console.error();
+      }
+    }
+  }
+
+  async function getColor() {
     try {
       const response = await fetch(
         "https://x-colors.yurace.pro/api/random/?type=dark"
       );
 
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
+      if (!response.ok) throw new Error(`Response status: ${response.status}`);
 
-      const result = await response.json();
-      setColor(result.hex);
+      return (await response.json()).hex;
     } catch (error) {
       console.error(error);
-    } finally {
-      setLoading(false);
+      return null;
     }
-  };
+  }
 
   useEffect(() => {
-    getColor();
-  }, []);
-
-  useEffect(() => {
+    getColor().then((color) => setColor(color));
     const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-
-    // canvas.style.width = window.innerWidth;
-    // canvas.style.height = window.innerHeight - 200;
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight - 200;
 
     let penDown = false;
     let prevPoint = { x: 0, y: 0 };
 
-    const draw = (e) => {
-      if (!loading && penDown) {
-        ctx.strokeStyle = color;
-        ctx.lineWidth = 10;
-        ctx.lineCap = "round";
-        ctx.lineJoin = "round";
-        ctx.beginPath();
-        ctx.moveTo(
-          prevPoint.x - canvas.offsetLeft,
-          prevPoint.y - canvas.offsetTop
-        );
-        ctx.lineTo(e.pageX - canvas.offsetLeft, e.pageY - canvas.offsetTop);
-        ctx.stroke();
+    function draw(x, y) {
+      const start = {
+        x: prevPoint.x - canvas.offsetLeft,
+        y: prevPoint.y - canvas.offsetTop,
+      };
+
+      const end = {
+        x: x - canvas.offsetLeft,
+        y: y - canvas.offsetTop,
+      };
+
+      if (penDown && connection) {
+        drawStroke(start, end, "#ffffff");
+        connection.invoke("Drawing", start, end, color);
       }
-      prevPoint = { x: e.pageX, y: e.pageY };
-    };
+      prevPoint = { x: x, y: y };
+    }
 
     canvas.addEventListener("mousedown", () => (penDown = true));
-    canvas.addEventListener("mousemove", (e) => draw(e));
+    canvas.addEventListener("mousemove", (e) => draw(e.pageX, e.pageY));
     canvas.addEventListener("mouseup", () => (penDown = false));
 
     return () => {
       canvas.addEventListener("mousedown", () => (penDown = true));
-      canvas.addEventListener("mousemove", (e) => draw(e));
+      canvas.addEventListener("mousemove", (e) => draw(e.pageX, e.pageY));
       canvas.addEventListener("mouseup", () => (penDown = false));
     };
-  }, [color]);
+  }, [connection]);
 
+  function drawStroke(start, end, color) {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 5;
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.beginPath();
+    ctx.moveTo(start.x, start.y);
+    ctx.lineTo(end.x, end.y);
+    ctx.stroke();
+  }
   return (
     <>
       <div>
+        <button onClick={startConnection}>Starta</button>
         <canvas ref={canvasRef} style={{ border: "1px solid white" }}></canvas>
       </div>
     </>
