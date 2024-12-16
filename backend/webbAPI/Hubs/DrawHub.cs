6e9f69f3195dd40543adb42c916028325a93a1bf
Saga-Lannerhjelm <saga.lannerhjelm@icloud.com>
2024-12-16 13:20:16 +0100

    Create Hub and Point class

--- backend/webbAPI/Hubs/DrawHub.cs
+++ backend/webbAPI/Hubs/DrawHub.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using webbAPI.Models;
 
 namespace webbAPI.Hubs
 {
-    public class DrawHub
+    public class DrawHub : Hub
     {
-        
+        public async Task Drawing(Point start, Point end) {
+            await Clients.Others.SendAsync("Drawing", start, end);
+        }
     }
 }
---
+++ backend/webbAPI/Models/Point.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webbAPI.Models
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
